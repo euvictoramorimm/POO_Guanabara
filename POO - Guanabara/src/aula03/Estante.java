fadac1bf5793bf9cb9507f6bc61f48e27a7c8909
Victor de Amorim Rodrigues <euvictoramorimm@users.noreply.github.com>
2025-09-04 09:59:51 -0300

    Create Estante.java

---
+++ POO - Guanabara/src/aula03/Estante.java
@@ -0,0 +1,17 @@
+package aula03;
+
+public class Estante {
+
+    //atributtes
+    public String cor;
+    public String material;
+    public int prateleiras;
+    public boolean vazia;
+
+    //methods
+    void status() {
+        Syste
+    }
+
+    
+}
